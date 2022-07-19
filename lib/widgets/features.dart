2ebcb32e19d6b1ffdfb17baf355b572435b14ff7
jiyuujin <jiyuujinunite@gmail.com>
2022-07-19 22:12:44 +0900

    announceSponsor to true

--- lib/widgets/features.dart
+++ lib/widgets/features.dart
@@ -1,11 +1,11 @@
 /// 初期ローンチ
-const initialLaunched = true;
+const initialLaunched = false;
 
 /// セッション公募の開始
 const startSession = false;
 
 /// スポンサー公募の周知
-const announceSponsor = false;
+const announceSponsor = true;
 
 /// スポンサー公募の開始 (前提として上記周知のフラグを true にする)
 const startSponsor = false;
