eb86e17939450cd01d1d99547814358747f26c4d
tameto <tameto@blueish.co.jp>
2022-07-21 16:14:17 +0900

    Added tameto profile image

--- lib/data/staff.dart
+++ lib/data/staff.dart
@@ -76,4 +76,10 @@ final kStaffList = [
         'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
     'url': 'https://twitter.com/katsummy',
   },
+  {
+    'name': '為藤アキラ',
+    'photo':
+        'https://pbs.twimg.com/profile_images/1017186161024856065/zI2avEVC_400x400.jpg',
+    'url': 'https://twitter.com/AkiraTameto',
+  },
 ];
