90b537cc8beedb31ac6ffdb25e8b4dd13443c0a9
jiyuujin <jiyuujin@users.noreply.github.com>
2022-08-08 03:14:37 +0000

    deploy: c56bce26952157d8487cf407a09dd978a5b0dd1b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "index.html": "4cc54b63440da084f3ae2fb324bf94c5",
 "/": "4cc54b63440da084f3ae2fb324bf94c5",
 "favicon.png": "6ca60969d955557b7886c622423ef486",
-"main.dart.js": "bbe50cdc209d03f809f6bf17b2598d4c",
+"main.dart.js": "2e2c4812eb5e4e9f7d2354dad109be65",
 "manifest.json": "e21a8991a312b40b1643de826961b249",
 "service-worker.js": "8e80dc880b1e22a2e73ff409d4b1d33e",
 "version.json": "f5952b3a36d0e2f8a98ed39dcc2f8afc",
