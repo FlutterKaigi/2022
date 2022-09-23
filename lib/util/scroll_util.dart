1c7458a0900f3c58c1684c1ef8ab809b231ea034
yama-yeah <b1021091@fun.ac.jp>
2022-09-24 08:32:31 +0900

    add error message

--- lib/util/scroll_util.dart
+++ lib/util/scroll_util.dart
@@ -3,7 +3,8 @@ import 'package:flutter/cupertino.dart';
 Future<void> animationScroll(Object anchor) async {
   final context = GlobalObjectKey(anchor).currentContext;
   if (context == null) {
-    throw Error();
+    throw ErrorDescription(
+        "An unregistered object was passed in animationScroll.\nYou should register GlobalObjectKey for some widget.");
   }
   await Scrollable.ensureVisible(context,
       duration: const Duration(milliseconds: 1000),
