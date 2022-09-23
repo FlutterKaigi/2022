c43689d3e1a2a359a272ced7788d46215f2d4152
yama-yeah <b1021091@fun.ac.jp>
2022-09-24 08:43:08 +0900

    add comment about animation scroll

--- lib/util/scroll_util.dart
+++ lib/util/scroll_util.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
+/// This method can scroll to a specific widget
+///
+/// Usage
+/// ```
+/// final anchor="This is example anchor";
+/// ~~~~~
+/// AnyButton(onPressed:() async => await animationScroll(anchor)),
+/// ~~~~~
+/// AnyWidget(key:GlobalObjectKey(anchor)),
+/// ```
 Future<void> animationScroll(Object anchor) async {
   final context = GlobalObjectKey(anchor).currentContext;
   if (context == null) {
